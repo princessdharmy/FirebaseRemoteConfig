f28853cc22748d74e65639c34d84f23553dd3192
damomos <damomos123@gmail.com>
2017-09-27 13:07:16 +0100

    Applied Changes

--- app/src/main/java/android/example/com/firebaseremoteconfig/MyFirebaseMessagingService.java
+++ app/src/main/java/android/example/com/firebaseremoteconfig/MyFirebaseMessagingService.java
@@ -24,12 +24,12 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         //Create Notification
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1410,
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1,
                 intent, PendingIntent.FLAG_ONE_SHOT);
 
         NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new
                 NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.images)
+                .setSmallIcon(R.drawable.icon)
                 .setContentTitle("Message")
                 .setContentText(remoteMessage.getNotification().getBody())
                 .setAutoCancel(true)
@@ -37,6 +37,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         NotificationManager notificationManager = (NotificationManager)
                 getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1410, notificationBuilder.build());
+        notificationManager.notify(1, notificationBuilder.build());
     }
 }
